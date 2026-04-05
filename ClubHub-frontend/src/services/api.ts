5e8401c5ae46f8814aac96502a9f2ee2d27418b9
DiogoRodriguesAB <diogorodriguesab@gmail.com>
2026-04-05 15:11:13 +0100

    feat: implement token-based authentication for scrapper API requests and handle token refresh on 401 errors

--- ClubHub-frontend/src/services/api.ts
+++ ClubHub-frontend/src/services/api.ts
@@ -27,6 +27,16 @@ api.interceptors.request.use(async (config) => {
   return config;
 });
 
+scrapperApi.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem("accessToken");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 // ✅ RESPONSE
 api.interceptors.response.use(
   (res) => res,
@@ -51,4 +61,29 @@ api.interceptors.response.use(
 
     return Promise.reject(error);
   },
+);
+
+scrapperApi.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const original = error.config;
+
+    if (error.response?.status === 401 && !original._retry) {
+      original._retry = true;
+
+      const refresh = await AsyncStorage.getItem("refreshToken");
+      if (!refresh) return Promise.reject(error);
+
+      const data = await refreshToken(refresh);
+
+      await AsyncStorage.setItem("accessToken", data.accessToken);
+      await AsyncStorage.setItem("refreshToken", data.refreshToken);
+
+      original.headers.Authorization = `Bearer ${data.accessToken}`;
+
+      return scrapperApi(original);
+    }
+
+    return Promise.reject(error);
+  }
 );
