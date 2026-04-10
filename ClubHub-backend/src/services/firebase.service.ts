5bcd0b87bfd808c75e951ce6061f958bdd6b769d
DiogoRodriguesAB <diogorodriguesab@gmail.com>
2026-04-11 00:53:07 +0100

    feat: implement Firebase service initialization with service account from secrets

--- ClubHub-backend/src/services/firebase.service.ts
+++ ClubHub-backend/src/services/firebase.service.ts
@@ -1,4 +1,20 @@
 import admin from "firebase-admin";
+import fs from "fs";
+
+const serviceAccount = JSON.parse(
+  fs.readFileSync("/etc/secrets/firebase.json", "utf8")
+);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+export default admin;
+
+/*
+import admin from "firebase-admin";
 import serviceAccount from "../config/adecas-70555-firebase-adminsdk-fbsvc-bf0ae60726.json";
 
 if (!admin.apps.length) {
@@ -8,3 +24,4 @@ if (!admin.apps.length) {
 }
 
 export default admin;
+*/
