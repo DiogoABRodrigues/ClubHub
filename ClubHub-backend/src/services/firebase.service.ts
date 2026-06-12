8d89a5025284abdf9be4965cd17161b8daf11e7e
DiogoRodriguesAB <diogorodriguesab@gmail.com>
2026-06-12 22:40:38 +0100

    refactor: clean up commented code in firebase service

--- ClubHub-backend/src/services/firebase.service.ts
+++ ClubHub-backend/src/services/firebase.service.ts
@@ -1,4 +1,4 @@
-/*import admin from "firebase-admin";
+import admin from "firebase-admin";
 import fs from "fs";
 
 const serviceAccount = JSON.parse(
@@ -11,10 +11,10 @@ if (!admin.apps.length) {
   });
 }
 
-export default admin;*/
+export default admin;
 
 
-import admin from "firebase-admin";
+/*import admin from "firebase-admin";
 import serviceAccount from "../config/adecas-70555-firebase-adminsdk-fbsvc-bf0ae60726.json";
 
 if (!admin.apps.length) {
@@ -23,4 +23,4 @@ if (!admin.apps.length) {
   });
 }
 
-export default admin;
+export default admin;*/
