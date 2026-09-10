017bad596ec6aafdba51bf8cca76570b584f09ab
DiogoRodriguesAB <diogorodriguesab@gmail.com>
2026-09-11 00:35:48 +0100

    fix: correct display of player names in SubstitutionLabel component

--- ClubHub-frontend/src/components/EventRow.tsx
+++ ClubHub-frontend/src/components/EventRow.tsx
@@ -121,11 +121,11 @@ const SubstitutionLabel = ({
     <Text numberOfLines={1}>
       {outFirst ? (
         <>
-          {outName} <Text style={styles.eventAssist}>({inName})</Text>
+          {outName} <Text style={styles.eventAssist}>{inName}</Text>
         </>
       ) : (
         <>
-          {inName} <Text style={styles.eventAssist}>({outName})</Text>
+          {inName} <Text style={styles.eventAssist}>{outName}</Text>
         </>
       )}
     </Text>
